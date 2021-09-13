9c14c1b49356b9e71888a462f22c77584a508c37
Christian Merrill <37678080+M-Chimiste@users.noreply.github.com>
2021-09-12 22:43:55 -0400

    Update track-buddy-mk2.ino

--- src/track-buddy/track-buddy-mk2/track-buddy-mk2.ino
+++ src/track-buddy/track-buddy-mk2/track-buddy-mk2.ino
@@ -1,8 +1,11 @@
 // Track Buddy Firmware Mk2
 // Author: Christian Merrill
 // License: MIT 2.0
-// Version: 0.1
+// Version: 0.2
 // Tested on Arduino Pro Micro with BNO080 and IDE 1.8.15
+// Changes in 0.2: Removed tare logic to opt for a joystick button press.
+// This allows for using native OpenTrack "center" feature which is more preferable.
+// Changed rotation vector to game rotation vector.
 
 #include <Wire.h>
 #include <Joystick.h>
@@ -11,8 +14,8 @@
 
 // You can use ANY of the pins for the buttons for the exception of the SDA and SCL pins
 // You can even change the arrangement of the buttons through updating these definitions (e.g. you wanted tare in back versus front etc)
-#define tareButton 8
-#define pauseButton 10
+#define tareButton 10
+#define pauseButton 9
 
 int currentTareState = 0;
 int currentPauseState;
@@ -26,13 +29,13 @@ int rzAxis_ = 0;
 float roll = 0;
 float yaw = 0;
 float pitch = 0;
-float xOffset = 0;
-float yOffset = 0;
-float rzOffset = 0;
+
 
 bool paused = false;
+bool tare = false;
 
 unsigned long lastPauseDebounceTime = 0;
+unsigned long lastTareDebounceTime = 0;
 
 const bool debug = false;  // enables serial printing if True (outputs degrees not ints)
 const bool initAutoSendState = false;  // used to automatically send joystick state
@@ -62,7 +65,7 @@ const int pauseDebounceTime = 50;
 //Include Brake: Determines whether a Brake axis is avalible for used by the HID system, defined as a bool value (default:true)
 //Include Steering: Determines whether a Steering axis is avalible for used by the HID system, defined as a bool value (default:true)
 
-Joystick_ Joystick(0x16, JOYSTICK_TYPE_JOYSTICK, 0, 0, true, true, false, false, false, true, false, false, false, false, false);  //Make sure that hex value is unique, update as needed.
+Joystick_ Joystick(0x16, JOYSTICK_TYPE_JOYSTICK, 1, 0, true, true, false, false, false, true, false, false, false, false, false);  //Make sure that hex value is unique, update as needed.
 
 BNO080 BNO080_;
 // Constants for Program
@@ -97,6 +100,20 @@ void setup()
   }
 }
 
+int setButtonState(int currentButtonState, int previousButtonState, int button){
+  // Function to update button states and update the joystick
+  int newButtonState;
+  if (currentButtonState != previousButtonState){
+    Joystick.setButton(button, currentButtonState);
+    newButtonState = currentButtonState;
+    return newButtonState;
+  }
+  else {
+    newButtonState = previousButtonState;
+    return newButtonState;
+  }
+}
+
 void loop()
 {
 
@@ -109,22 +126,21 @@ void loop()
       pitch = (BNO080_.getPitch()) * 180.0 / PI; // Convert pitch to degrees
       yaw = (BNO080_.getYaw()) * 180.0 / PI; // Convert yaw / heading to degrees
 
-      // Apply offset to roll/pitch/yaw
-      roll = roll - xOffset;
-      pitch = pitch - yOffset;
-      yaw = yaw - rzOffset;
+
     
       // Check for tare button push
       currentTareState = !digitalRead(tareButton);
+      lastTareState = setButtonState(currentTareState, lastTareState, 0);
+
 
       // debouncing not necessary for tare button
-      if (currentTareState != lastTareState) {
-        // set current axis values as offset from internal measures of current values
-        xOffset = (BNO080_.getRoll()) * 180.0 / PI;
-        yOffset = (BNO080_.getPitch()) * 180.0 / PI;
-        rzOffset = (BNO080_.getYaw()) * 180.0 / PI;
-    
-    }
+//      if (currentTareState != lastTareState) {
+//        // set current axis values as offset from internal measures of current values
+//        xOffset = (BNO080_.getRoll()) * 180.0 / PI;
+//        yOffset = (BNO080_.getPitch()) * 180.0 / PI;
+//        rzOffset = (BNO080_.getYaw()) * 180.0 / PI;
+//    
+//    }
     }
     
    
