a4f4e8262b514515d030f8e8ae3f6dbb55d27350
Christian Merrill <37678080+M-Chimiste@users.noreply.github.com>
2021-09-14 08:50:28 -0400

    Update track-buddy-mk2.ino
    removed pausing logic and converted pause button into a joystick button.

--- src/track-buddy/track-buddy-mk2/track-buddy-mk2.ino
+++ src/track-buddy/track-buddy-mk2/track-buddy-mk2.ino
@@ -21,7 +21,6 @@ int currentTareState = 0;
 int currentPauseState;
 int lastTareState = 0;
 int lastPauseState = LOW;
-int lastPauseCHGState = LOW;
 int xAxis_ = 0;
 int yAxis_ = 0;
 int rzAxis_ = 0;
@@ -30,20 +29,11 @@ float roll = 0;
 float yaw = 0;
 float pitch = 0;
 
-
-bool paused = false;
-bool tare = false;
-
-unsigned long lastPauseDebounceTime = 0;
-unsigned long lastTareDebounceTime = 0;
-
 const bool debug = false;  // enables serial printing if True (outputs degrees not ints)
 const bool initAutoSendState = false;  // used to automatically send joystick state
 const int smoothing = 1023; // used to map roll/pitch/yaw to an integer value 
 const int debounce = 1;  // delay in ms for debouncing
 const int sendRate = 1; // send rate in ms for the BNO080 IMU;
-const int pauseDebounceTime = 50;
-
 
 
 // Initalize the Joystick Data Structure
@@ -65,7 +55,7 @@ const int pauseDebounceTime = 50;
 //Include Brake: Determines whether a Brake axis is avalible for used by the HID system, defined as a bool value (default:true)
 //Include Steering: Determines whether a Steering axis is avalible for used by the HID system, defined as a bool value (default:true)
 
-Joystick_ Joystick(0x16, JOYSTICK_TYPE_JOYSTICK, 1, 0, true, true, false, false, false, true, false, false, false, false, false);  //Make sure that hex value is unique, update as needed.
+Joystick_ Joystick(0x16, JOYSTICK_TYPE_JOYSTICK, 2, 0, true, true, false, false, false, true, false, false, false, false, false);  //Make sure that hex value is unique, update as needed.
 
 BNO080 BNO080_;
 // Constants for Program
@@ -120,7 +110,6 @@ void loop()
   if (BNO080_.dataAvailable() == true)
   {
     
-    if (paused != true){
       // Get raw values from sensor, example taken from Spark Fun library
       roll = (BNO080_.getRoll()) * 180.0 / PI; // Convert roll to degrees
       pitch = (BNO080_.getPitch()) * 180.0 / PI; // Convert pitch to degrees
@@ -132,19 +121,12 @@ void loop()
       currentTareState = !digitalRead(tareButton);
       lastTareState = setButtonState(currentTareState, lastTareState, 0);
 
+      currentPauseState = !digitalRead(pauseButton);
+      lastPauseState = setButtonState(currentPauseState, lastPauseState, 1);
 
-      // debouncing not necessary for tare button
-//      if (currentTareState != lastTareState) {
-//        // set current axis values as offset from internal measures of current values
-//        xOffset = (BNO080_.getRoll()) * 180.0 / PI;
-//        yOffset = (BNO080_.getPitch()) * 180.0 / PI;
-//        rzOffset = (BNO080_.getYaw()) * 180.0 / PI;
-//    
-//    }
-    }
+    
     
    
-
     // Convert roll/pitch/yaw into integers for sending to joystick axis values
     xAxis_ = map(roll, -180, 180, 0, smoothing);
     yAxis_ = map(pitch, -180, 180, 0, smoothing);
@@ -154,26 +136,7 @@ void loop()
     Joystick.setXAxis(xAxis_);
     Joystick.setYAxis(yAxis_);
     Joystick.setRzAxis(rzAxis_);
-  
-    
-    
-    currentPauseState = digitalRead(pauseButton);
-    
-    if (currentPauseState != lastPauseCHGState) {
-      lastPauseDebounceTime = millis();
-      lastPauseCHGState = currentPauseState;
-    }
 
-    if ((millis() - lastPauseDebounceTime) > pauseDebounceTime) {
-      if (lastPauseState == HIGH && currentPauseState == LOW) {
-        paused = !paused;
-      }
-
-
-      lastPauseState = currentPauseState;
-    }
-
-    
     
   if (debug == true){
       Serial.print(roll, 1);
